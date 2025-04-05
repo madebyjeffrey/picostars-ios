a7dd461b157979ba906ed9c2af5cc79f3879b1bb
Jeffrey Drake <jeff@ideaplex.ca>
2025-04-05 02:17:52 -0600

    Initial Commit

---
+++ picostars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  picostars
+//
+//  Created by Jeffrey Drake on 2025-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ picostars/picostarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  picostarsApp.swift
+//  picostars
+//
+//  Created by Jeffrey Drake on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct picostarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
